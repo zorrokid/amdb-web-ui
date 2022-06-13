d52b57f719264436d47d22842874c3d5b779dba2
Mikko Keinänen <mikko.keinanen@tietoevry.com>
2022-06-13 14:36:01 +0300

    small layout fixes

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,3 +1,3 @@
 export default function Footer() {
-    return <>Footer</>;
+    return <section>Footer</section>;
 }
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,10 +2,14 @@ import { Link } from "react-router-dom";
 
 export default function Header() {
     return (
-        <nav>
-            <Link to="/">Main</Link>
-            <Link to="/movies">Movies</Link>
-            <Link to="/releases">Releases</Link>
-        </nav>
+        <section>
+            <nav>
+                <ul>
+                    <li><Link to="/">Main</Link></li>
+                    <li><Link to="/movies">Movies</Link></li>
+                    <li><Link to="/releases">Releases</Link></li>
+                </ul>
+            </nav>
+        </section>
     );
 }
